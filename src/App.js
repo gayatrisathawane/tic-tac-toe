7490db443dcd1d18cad2ab83ce708a59e6345004
gayatrisathawane <gayatripsathawane@gmail.com>
2023-11-23 16:13:32 +0530

    checkwinner function implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from './images.png'
 import './App.css'
 
@@ -18,22 +18,75 @@ const App = () => {
     9: "",
 
   })
+  const [winner, setWinner] = useState(null)
 
+  const checkWinner = (player) => {
 
-  const play = (boxNo)=>{
-    if(board[boxNo] !== "") {
-      alert("already filled")
-      return
+    const symbol = (player === 1 ? '❌' : '⭕')
+
+    if (board[1] === symbol && board[2] === symbol && board[3] === symbol) {
+
+      setWinner(player)
     }
-  if(player===1){
-    setBoard({...board,[boxNo]:'❌'})
-  }
-  else{
-     setBoard({...board,[boxNo]:'⭕'})
+    else if (board[4] === symbol && board[5] === symbol && board[6] === symbol) {
 
-  }
-  setPlayer(player===1 ?2:1)
+      setWinner(player)
+    }
+    else if (board[7] === symbol && board[8] === symbol && board[9] === symbol) {
 
+      setWinner(player)
+    }
+    else if (board[1] === symbol && board[4] === symbol && board[7] === symbol) {
+
+      setWinner(player)
+
+    }
+    else if (board[2] === symbol && board[5] === symbol && board[8] === symbol) {
+
+      setWinner(player)
+    }
+    else if (board[3] === symbol && board[6] === symbol && board[9] === symbol) {
+
+      setWinner(player)
+    }
+    else if (board[1] === symbol && board[5] === symbol && board[9] === symbol) {
+
+      setWinner(player)
+    }
+    else if (board[3] === symbol && board[5] === symbol && board[7] === symbol) {
+
+      setWinner(player)
+
+    }
+
+    else {
+      return;
+    }
+  }
+
+
+  const play = (boxNo) => {
+    if (board[boxNo] !== "" || winner !== null) {
+     
+      return;
+    }
+    if (player === 1) {
+      setBoard({ ...board, [boxNo]: '❌' })
+    }
+    else {
+      setBoard({ ...board, [boxNo]: '⭕' })
+
+    }
+  }
+
+
+  useEffect(() => {
+    checkWinner(player)
+    setPlayer(player === 1 ? 2 : 1)
+  }, [board])
+
+  const reset = ()=>{
+    
   }
   return (
     <div>
@@ -41,36 +94,46 @@ const App = () => {
       <img src={logo} alt="logo" className='logo' />
       <h1 className='text-center text'>Let's Play The Tic-Tac-Toc Game ! </h1>
 
-      <button className='btn-new-game'>Start a new game</button>
+      {/* <button className='btn-new-game'>Start a new game</button> */}
 
       <div className='player-container'>
-        <div className='player-text'>player 1: ❌ </div>
-        <span className='player-text'>current player :{player ===1 ? '❌' :'⭕'}</span>
-        <span className='player-text'>player 2:  ⭕ </span>
+        <div className='player-text'>Player 1: ❌ </div>
+        <span className='player-text'>current player :{player === 1 ? '❌' : '⭕'}</span>
+        <span className='player-text'>Player 2:  ⭕ </span>
+      </div>
+
+      <div>
+        <p className='winner-div'>
+          {winner? 
+          (
+            <h2 className='text-center'>🎉🎊 congratulation you are win 🥳🥳🎊 {winner===1? '❌':'⭕'}
+            
+            </h2>):null}
+        </p>
       </div>
 
 
       <div className='container'>
 
         <div className='row'>
-          <div className='box' onClick={()=>{play(1)}}>{board[1]}</div>
-          <div className='box' onClick={()=>{play(2)}}>{board[2]}</div>
-          <div className='box' onClick={()=>{play(3)}}>{board[3]}</div>
+          <div className='box' onClick={() => { play(1) }}>{board[1]}</div>
+          <div className='box' onClick={() => { play(2) }}>{board[2]}</div>
+          <div className='box' onClick={() => { play(3) }}>{board[3]}</div>
 
         </div>
 
         <div className='row'>
-          <div className='box'onClick={()=>{play(4)}}>{board[4]}</div>
-          <div className='box'onClick={()=>{play(5)}}>{board[5]}</div>
-          <div className='box'onClick={()=>{play(6)}}>{board[6]}</div>
+          <div className='box' onClick={() => { play(4) }}>{board[4]}</div>
+          <div className='box' onClick={() => { play(5) }}>{board[5]}</div>
+          <div className='box' onClick={() => { play(6) }}>{board[6]}</div>
 
         </div>
 
 
         <div className='row'>
-          <div className='box'onClick={()=>{play(7)}}>{board[7]}</div>
-          <div className='box'onClick={()=>{play(8)}}>{board[8]}</div>
-          <div className='box'onClick={()=>{play(9)}}>{board[9]}</div>
+          <div className='box' onClick={() => { play(7) }}>{board[7]}</div>
+          <div className='box' onClick={() => { play(8) }}>{board[8]}</div>
+          <div className='box' onClick={() => { play(9) }}>{board[9]}</div>
 
         </div>
 
